c1524306b5f519ffff78df5b288d4194da506079
Wilson Gonzalez <wilson1234567891962@hotmail.com>
2022-07-06 19:52:26 -0500

    Prueba

--- realtimeMonitoring/realtimeGraph/urls.py
+++ realtimeMonitoring/realtimeGraph/urls.py
@@ -4,6 +4,6 @@ from django.views.decorators.csrf import csrf_exempt
 from .views import *
 
 urlpatterns = [
-    path('', TestData.as_view(), name='getTestData'),
+    path('aaaaaaa', TestData.as_view(), name='getTestData'),
     path('historical/', HistoricalView.as_view(), name='historical'),
 ]
