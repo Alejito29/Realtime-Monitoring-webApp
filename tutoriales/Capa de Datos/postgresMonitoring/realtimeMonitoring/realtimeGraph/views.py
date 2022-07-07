fdc56025050c5ef828e52abe04cefec26c8ca995
Wilson Gonzalez <wilson1234567891962@hotmail.com>
2022-07-07 18:23:41 -0500

    Update the version

--- tutoriales/Capa de Datos/postgresMonitoring/realtimeMonitoring/realtimeGraph/views.py
+++ tutoriales/Capa de Datos/postgresMonitoring/realtimeMonitoring/realtimeGraph/views.py
@@ -39,7 +39,7 @@ class DashboardView(TemplateView):
         try:
             cityO = City.objects.all()
             stateO = State.objects.all()
-            countryO = Country.objects
+            countryO = Country.objects.all()
             tmpJson_cityO = serializers.serialize("json", cityO)
             tmpJson_stateO = serializers.serialize("json", stateO)
             tmpJson_countryO = serializers.serialize("json", countryO)
